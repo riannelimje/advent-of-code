3bdbd682f3560fac2ef846e1fcc1c2666c77f18b
Rianne Lim <rianne.limje@gmail.com>
2024-12-17 12:28:57 +0800

    day7 part 1

---
+++ day7/7.py
@@ -0,0 +1,27 @@
+from itertools import product
+
+with open('input.txt', 'r') as file:
+    data = file.read().strip().splitlines()
+
+def evaluate(numbers, item):
+    res = numbers[0]
+    for i in range(len(item)):
+        if item[i] == '+':
+            res += numbers[i+1]
+        else:
+            res *= numbers[i+1]
+    return res
+
+total = 0
+for line in data:
+    testValue, numbers = line.strip().split(":")
+    testValue = int(testValue)
+    numbers = list(map(int, numbers.strip().split()))
+
+    # i need to generate all possible combinations
+    for combi in product('+*', repeat=len(numbers)-1): #('+',), ('*',)
+        if evaluate(numbers, combi) == testValue:
+            total += testValue
+            break
+    
+print(total)
