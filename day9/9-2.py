9fb6718509ce7ec725f07cd1b99dc6aa02f18393
Rianne Lim <rianne.limje@gmail.com>
2024-12-19 15:00:17 +0800

    yay day 9 part 2 solved

--- day9/9-2.py
+++ day9/9-2.py
@@ -1,80 +1,42 @@
-with open('test.txt', 'r') as file:
+with open('input.txt', 'r') as file:
     data = file.read().strip()
 
-arr = []
-index = 0
+dictionary = {} # store the postion and the length of the number 
+blanks = [] # store position of each blanks
+pos = 0
+fid = 0
 
-for i in range(len(data)):
-    if i%2==0:
-        for i in range(int(data[i])):
-            arr.append(index)
-        index +=1
+for i, char in enumerate(data):
+    length = int(char)
+    if i % 2 == 0:
+        dictionary[fid] = (pos,length)
+        fid += 1
     else:
-        for i in range(int(data[i])):
-            arr.append('.')
+       if length != 0:
+            blanks.append((pos,length))
 
-# store a tuple (start, end) - [inclusive, exclusive) for the consecutive . 
-dots = []
-start = 0
-end = 0
-for i in range(len(arr)):
-    if arr[i] == '.':
-        start = i
-        for j in range(i, len(arr)):
-            if arr[j] != '.':
-                end = j
-                break
-        if dots == []:
-            dots.append((start, end))
-            continue
-        if start < dots[-1][-1]:
-            continue
-        dots.append((start, end))
-print("dots are")
-print(dots)
-print("--------")
-print("original arr")
-print(arr)
-print("--------")
-
-# find the length of the dots 
-def dotLen(length):
-    for start, end in dots:
-        dotLen = end - start + 1
-        if dotLen > length:
-            return start, end 
-    return 0,0
-
-# print(dots)
-dictionary = {}
-length = 0
-# get the length of all the numbers put in a dictionary maybe 
-for i in range(len(arr)-1, -1, -1):
-    if arr[i] != '.':
-        num = arr[i]
-        if num not in dictionary:
-            dictionary[num] = 1 
-        if num == arr[i-1]:
-            dictionary[num] += 1
+    pos += length
 
 
-# find the corresponding dots to the length 
-for key, value in dictionary.items():
-    if dotLen(value) != (0,0):
-        # print(value)
-        # print(dotLen(value))
-        # print("went in")
-        start, end = dotLen(value)
-        dots.remove((start,end))
-        # have to also add back the dots if it is not being used
-        if end >= start + value:
-            dots.insert(0, (start+value, end))
-        # print(dots)
-        # print(start)
-        # print(end)
-        arr[start:start+value] = [key] * value
-        print(arr)
-        
-        # need to also find a way to delete the elements that are swapped
+#curr fid will be at the highest one
+while fid > 0:
+    fid -= 1 # need to account for the fid+1 at line 14 and decrement it each time 
+    pos, length  = dictionary[fid]
+    for i, (start, bLen) in enumerate(blanks):
+        # if the dot goes pass the position of the fid - means it doesn't apply to it anymore
+        if start >= pos:
+            break
+        if length <= bLen:
+            dictionary[fid] = (start, length) #replace with the new one
+            if length == bLen:
+                blanks.pop(i) # delete the whole tuple
+            else:
+                blanks[i] = (start+length,bLen-length) # replace with the new dot locations 
+            break
 
-print(dictionary)
+sum = 0
+for fid, (pos,length) in dictionary.items():
+    for i in range(pos, pos+length):
+        sum += fid * i
+
+print(sum)
